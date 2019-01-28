46f9b3f0ab4d0a7bd51ea516ae27d230e8d54df7
Paul Lemoine <paul.lemoine.1@gmail.com>
2019-01-28 09:31:46 +0100

    fixing pool connection.

--- src/model/index.js
+++ src/model/index.js
@@ -5,14 +5,24 @@ const Sequelize = require('sequelize');
 const db = {};
 const basename = path.basename(module.filename);
 
-const sequelize = new Sequelize(process.env.DATABASE_URL);
+//const sequelize = new Sequelize(process.env.DATABASE_URL);
+const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PASSWORD, {
+  host: process.env.DATABASE_URL,
+  dialect: 'postgres',
+  pool: {
+    idle: 30000,
+    min: 0,
+    max: 5
+  }
+});
+
 fs.readdirSync(__dirname)
   .filter(
     file =>
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('triggers') === -1
+    file.indexOf('.') !== 0 &&
+    file !== basename &&
+    file.slice(-3) === '.js' &&
+    file.indexOf('triggers') === -1
   )
   .forEach(file => {
     const model = sequelize.import(path.join(__dirname, file));
